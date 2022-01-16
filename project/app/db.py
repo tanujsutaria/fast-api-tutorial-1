82186bff4e74de3f866d0055c32f31ebe30cccc0
Tanuj Sutaria <tanujsutaria@Tanujs-MacBook-Pro-2.local>
2022-01-15 21:22:02 -0800

    Added the register tortoise function to the db file and removed it from main

--- project/app/db.py
+++ project/app/db.py
@@ -1,5 +1,8 @@
 import os
 
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
     "apps": {
@@ -9,3 +12,12 @@ TORTOISE_ORM = {
         },
     },
 }
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url = os.environ.get("DATABASE_URL"),
+        modules = {"models": ["app.models.tortoise"]},
+        generate_schemas = False,
+        add_exception_handlers= True
+    )
