919755de1c749a4195966e07ed607afa21a729d9
Tanuj Sutaria <tanujsutaria@gmail.com>
2022-01-16 17:43:53 -0800

    Updated test for POST routes, all pass

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -6,6 +6,8 @@ from starlette.testclient import TestClient
 from app.main import create_application
 from app.config import get_settings, Settings
 
+from tortoise.contrib.fastapi import register_tortoise
+
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
@@ -13,9 +15,33 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    app = create_application() 
+    # set up
+    app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
 
-        
+        # testing
         yield test_client
+
+    # tear down
+
+
+# new
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+
+        # testing
+        yield test_client
+
+    # tear down
