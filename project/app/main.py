534f85bceb7450acefc3250b7db52bdb028143a7
Tanuj Sutaria <tanujsutaria@gmail.com>
2022-01-16 17:07:44 -0800

    Commiting changes to main method that weren't commited as part of last push

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping, summaries  # updated
+from app.api import ping, summaries
 from app.db import init_db
 
 
@@ -12,7 +12,7 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
-    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
